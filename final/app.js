2f20293f792f0c5799dc921a35609ef11e0900e7
Lin Chen <lc546@bu.edu>
2018-06-27 00:27:54 -0400

    Updated
    Update

--- final/app.js
+++ final/app.js
@@ -1,52 +1,52 @@
-const express = require('express'); // Express web server framework
-const request = require('request-promise'); // "Request" library
-const querystring = require('querystring');
-const cookieParser = require('cookie-parser');
+var express = require('express'); // Express web server framework
+var request = require('request-promise'); // "Request" library
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
 
-const client_id = '8a5e7080b0ae41e18165590ff06592a3'; 
-const client_secret = '6343078f199a4eec9065ad35357210f7'; 
-const redirect_uri = 'http://localhost:8888/callback'; 
-const queryService = require('./service');
+var client_id = '8a5e7080b0ae41e18165590ff06592a3'; 
+var client_secret = '6343078f199a4eec9065ad35357210f7'; 
+var redirect_uri = 'http://localhost:8888/callback'; 
+var queryService = require('./service');
 
 //Set up database connection
-const mongoose = require('mongoose')
+var mongoose = require('mongoose')
 if (!mongoose.connection.db) {
     mongoose.connect('mongodb://localhost/day')
 }
 
-const db = mongoose.connection
+var db = mongoose.connection
 db.once('open', function () {
     console.log('Connected to database!')
 })
 
-const Schema = mongoose.Schema;
-const albumSchema = new Schema ({
+var Schema = mongoose.Schema;
+var albumSchema = new Schema ({
     art: {type: String, unqiue: false},
     name: {type: String, unique: false}
 })
 
-const albums = mongoose.model('albums', albumSchema);
+var albums = mongoose.model('albums', albumSchema);
 
-const howisyourday = require('./routes/howisyourday')
+var howisyourday = require('./routes/howisyourday')
 
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-const generateRandomString = function(length) {
-    const text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+var generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    for (const i = 0; i < length; i++) {
+    for (var i = 0; i < length; i++) {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
 };
 
-const stateKey = 'spotify_auth_state';
+var stateKey = 'spotify_auth_state';
 
-const app = express();
+var app = express();
 
 app.use(express.static(__dirname + '/public'))
     .use(cookieParser());
@@ -55,11 +55,11 @@ app.use('/howisyourday', howisyourday);
 
 app.get('/login', function(req, res) {
 
-    const state = generateRandomString(16);
+    var state = generateRandomString(16);
     res.cookie(stateKey, state);
 
     // application requests authorization
-    const scope = 'user-read-private user-read-email user-library-read';
+    var scope = 'user-read-private user-read-email user-library-read';
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
             response_type: 'code',
@@ -75,9 +75,9 @@ app.get('/callback', function(req, res) {
     // your application requests refresh and access tokens
     // after checking the state parameter
 
-    const code = req.query.code || null;
-    const state = req.query.state || null;
-    const storedState = req.cookies ? req.cookies[stateKey] : null;
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
 
     if (state === null || state !== storedState) {
         res.redirect('/#' +
@@ -86,7 +86,7 @@ app.get('/callback', function(req, res) {
             }));
     } else {
         res.clearCookie(stateKey);
-        const authOptions = {
+        var authOptions = {
             url: 'https://accounts.spotify.com/api/token',
             form: {
                 code: code,
@@ -102,10 +102,10 @@ app.get('/callback', function(req, res) {
         request.post(authOptions, function(error, response, body) {
             if (!error && response.statusCode === 200) {
 
-                const access_token = body.access_token,
+                var access_token = body.access_token,
                     refresh_token = body.refresh_token;
 
-                const options = {
+                var options = {
                     url: 'https://api.spotify.com/v1/me',
                     headers: { 'Authorization': 'Bearer ' + access_token },
                     json: true
@@ -168,8 +168,8 @@ app.get('/callback', function(req, res) {
 app.get('/refresh_token', function(req, res) {
 
     // requesting access token from refresh token
-    const refresh_token = req.query.refresh_token;
-    const authOptions = {
+    var refresh_token = req.query.refresh_token;
+    var authOptions = {
         url: 'https://accounts.spotify.com/api/token',
         headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
         form: {
@@ -181,7 +181,7 @@ app.get('/refresh_token', function(req, res) {
 
     request.post(authOptions, function(error, response, body) {
         if (!error && response.statusCode === 200) {
-            const access_token = body.access_token;
+            var access_token = body.access_token;
             res.send({
                 'access_token': access_token
             });
@@ -190,7 +190,7 @@ app.get('/refresh_token', function(req, res) {
 });
 
 app.get('/search', function(req, res) {
-    const text = req.query.text;
+    var text = req.query.text;
     queryService.search(text, (data) => {res.send(data); console.log(data)})
 })
 app.get('/top', function(req, res) {
